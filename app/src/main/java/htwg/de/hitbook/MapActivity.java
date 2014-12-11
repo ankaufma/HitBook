342048017bde00476b914be4e2781bc72d4c1604
ankaufma <ankaufma@htwg-konstanz.de>
2014-12-11 16:03:53 +0100

    example log

--- app/src/main/java/htwg/de/hitbook/MapActivity.java
+++ app/src/main/java/htwg/de/hitbook/MapActivity.java
@@ -2,6 +2,7 @@ package htwg.de.hitbook;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,6 +20,7 @@ public class MapActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_map, menu);
+        Log.d("MainActivity", "Wird On Create ausgeführt?");
         return true;
     }
 
