8e5ee320944533ba3374e705e71bf7e6cea42d2f
ankaufma <ankaufma@htwg-konstanz.de>
2014-12-16 17:03:12 +0100

    Added Camera Options, please change in your manfifestFile. Hitbook-Activity to start-Activity. Via Menu you can come to the MapActivity

--- app/src/main/java/htwg/de/hitbook/MapActivity.java
+++ app/src/main/java/htwg/de/hitbook/MapActivity.java
@@ -1,20 +1,53 @@
 package htwg.de.hitbook;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
 
 public class MapActivity extends ActionBarActivity {
+    GoogleMap map;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            //your code here
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        map = mapFragment.getMap();
+        map.setMyLocationEnabled(true);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -26,16 +59,13 @@ public class MapActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case (R.id.action_settings):
+                return true;
+            case (R.id.map):
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
